17c5ee3db4be686c9f71007b35d26883e2c338b9
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-05-16 16:38:21 -0300

    commit listas

---
+++ netflixclone/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+
+export default () => {
+  
+const [movieList, setMovieList] = useState([]);
+
+useEffect(()=>{
+
+  const loadAll = async () => {
+    //  Pegando a lista total
+    let list = await Tmdb.getHomeList();
+    setMovieList(list)
+  }
+
+  loadAll();
+}, []);
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            {item.title}
+          </div>
+        ))}
+      </section>
+    </div>
+  )
+}
